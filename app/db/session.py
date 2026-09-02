abd5a80c5dca755e8b7de32e131a4e51d6ea32a9
Marina.Zakharina <machm@mail.ru>
2026-09-02 10:50:54 +0300

    project structure & poetry dependencies & base settings for postgres & docker posgres up

---
+++ app/db/session.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.core.settings import settings
+
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://"
+    f"{settings.postgres_user}:{settings.postgres_password}"
+    f"@{settings.postgres_host}:{settings.postgres_port}"
+    f"/{settings.postgres_db}"
+)
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+)
+
+async_session_factory = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
